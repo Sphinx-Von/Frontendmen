61f09f0056e22fabbce86d23b426cccc68470180
Sphinx-Von <rubinahakim95@gmail.com>
2025-10-31 10:08:34 +0530

    ejs routing

---
+++ express/script.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+app.set("view engine", "ejs")
+app.use(express.static('./public'))
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3000)
