4da86cf3edf1aa2ca91cbdd8e3025112f653d243
Sphinx-Von <rubinahakim95@gmail.com>
2025-11-21 13:57:08 +0530

    f

--- codeatrandom/career-analyzer/server.js
+++ codeatrandom/career-analyzer/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Enable CORS
 app.use(cors({
-  origin: 'http://localhost:3000', // Your Next.js frontend URL
+  origin: '*', // Your Next.js frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
